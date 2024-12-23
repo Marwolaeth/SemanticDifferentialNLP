e377cad544d972a4f39a4a469a88808b564137ed
Culturology's Undead <a.pawluczenko@gmail.com>
2024-12-23 13:03:31 +0300

    Giraffe not needed for gauges

--- _gauge-test.R
+++ _gauge-test.R
@@ -1,21 +1,33 @@
 library(shiny)
 library(shinydashboard)
 library(flexdashboard)
+library(ggiraph)
 
 ui <- dashboardPage(
   dashboardHeader(title = "Bipolar Scale Visualization"),
-  dashboardSidebar(disable = TRUE),
+  dashboardSidebar(
+    sliderInput('value', 'Value', -100, 100, step = 1, value = 40)
+  ),
   dashboardBody(
     gaugeOutput("bipolar_gauge")
+    # girafeOutput('bipolar_gauge')
   )
 )
 
 server <- function(input, output) {
   output$bipolar_gauge <- renderGauge({
-    value <- 40  # Replace with your dynamic value
-    gauge(value, min = -100, max = 100, 
-          symbol = "%", 
-          gaugeSectors(success = c(50, 100), danger = c(-100, 0), warning = c(0, 50)))
+    req(input$value)
+    value <- input$value
+    ggauge <- gauge(value, min = -100, max = 100, 
+      symbol = "%", 
+      gaugeSectors(
+        success = c(50, 100),
+        danger = c(-100, 0),
+        warning = c(0, 50)
+      ),
+    )
+    return(ggauge)
+    # girafe(ggobj = ggauge)
   })
 }
 
